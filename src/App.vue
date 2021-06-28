e88dbb7fe9073d802ba5b4a9dd06bdb97e4f1567
Fedorae <services.fedorae@gmail.com>
2021-06-28 20:45:52 +0100

    Init Commit

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </div>
 </template>
 
