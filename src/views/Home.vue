4eed96de89dee9fdb321934e9284f46ec47074e4
Fedorae <services.fedorae@gmail.com>
2021-07-05 09:15:35 +0100

    How to add dynamic layouts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Dynamic Layouts Vue.js App"/>
   </div>
 </template>
 
@@ -9,10 +9,15 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+import Index from "@/layouts/Index.vue"
+
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created() {
+    this.$emit("update:layout", Index)
   }
 }
 </script>
